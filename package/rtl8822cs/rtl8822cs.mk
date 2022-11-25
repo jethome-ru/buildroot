8387237f3bdc0ae62e7dfc0641dffcf60ab90e60
Vyacheslav Bocharov <adeep@lexina.in>
2022-11-25 13:09:06 +0300

    rtl8822cs: new package
    This package adds the rtl88822cs WiFi driver.
       repo: https://github.com/jethome-ru/rtl88x2cs.git
       branch: tune_for_jethub
    
    Driver is known to support Realtek RTL8822CS SDIO WiFi/BT chip.
    
    Signed-off-by: Viacheslav Bocharov <adeep@lexina.in>

---
+++ package/rtl8822cs/rtl8822cs.mk
@@ -0,0 +1,26 @@
+################################################################################
+#
+# Realtek RTL8822CS driver
+#
+################################################################################
+
+RTL8822CS_VERSION = db8dc6c7ae1a75af3f6d7fa4f05456c76f5cab3e
+RTL8822CS_SITE = $(call github,jethome-ru,rtl88x2cs,$(RTL8822CS_VERSION))
+RTL8822CS_LICENSE = GPL-2.0
+
+RTL8822CS_MODULE_MAKE_OPTS = \
+	CONFIG_RTL8822CS=m \
+	KVER=$(LINUX_VERSION_PROBED) \
+	KSRC=$(LINUX_DIR)
+
+define RTL8822CS_LINUX_CONFIG_FIXUPS
+	$(call KCONFIG_ENABLE_OPT,CONFIG_NET)
+	$(call KCONFIG_ENABLE_OPT,CONFIG_WIRELESS)
+	$(call KCONFIG_ENABLE_OPT,CONFIG_CFG80211)
+	$(call KCONFIG_ENABLE_OPT,CONFIG_MAC80211)
+	$(call KCONFIG_ENABLE_OPT,CONFIG_MMC)
+endef
+
+
+$(eval $(kernel-module))
+$(eval $(generic-package))
